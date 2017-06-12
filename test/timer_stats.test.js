4819dcd7ab15bf846059e35f4dc359d689a3d736
Rafa de la Torre <rtorre@carto.com>
2017-06-12 16:46:55 +0200

    Improved test for TimerStats

--- test/timer_stats.test.js
+++ test/timer_stats.test.js
@@ -3,15 +3,45 @@
 var mapnik = require('../');
 var assert = require('assert');
 
-describe('timer_stats', function() {
+// register fonts and datasource plugins
+mapnik.register_default_fonts();
+mapnik.register_default_input_plugins();
 
-    it('flush should return an empty string', function(done) {
-        assert.equal(mapnik.TimerStats.flush(), "");
-        done();
-    });
+describe('TimerStats', function() {
 
-    it('test that you cant initialize a TimerStats', function() {
+    it('test that you cannot initialize a TimerStats', function() {
         assert.throws(function() { var l = new mapnik.TimerStats(); });
     });
 
+    describe('flush()', function() {
+        before(function(done) {
+            mapnik.TimerStats.flush();
+            done();
+        });
+
+        after(function(done) {
+            mapnik.TimerStats.flush();
+            done();
+        });
+
+
+        it('should return an empty string if no map rendered', function(done) {
+            assert.equal(mapnik.TimerStats.flush(), "");
+            done();
+        });
+
+        it('returns some timing stats after rendering a map', function(done) {
+            var map = new mapnik.Map(256, 256);
+            map.load('./test/stylesheet.xml', function(err,map) {
+                if (err) throw err;
+                map.zoomAll();
+                var im = new mapnik.Image(256, 256);
+                map.render(im, function(err,im) {
+                    if (err) throw err;
+                    assert.ok(/^total_map_rendering/.test(mapnik.TimerStats.flush()));
+                    done();
+                });
+            });
+        });
+    });
 });
