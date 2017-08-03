b855f5ff05cd80cf006cb6730df78453bf12f9f4
Rafa de la Torre <rtorre@carto.com>
2017-08-03 16:12:49 +0200

    Fix ABI version

--- build-linux.sh
+++ build-linux.sh
@@ -3,7 +3,7 @@ set -u -e
 
 export NODE_VERSION="6"
 
-CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0"
+export CXXFLAGS="${CXXFLAGS:-""} -D_GLIBCXX_USE_CXX11_ABI=0"
 
 git clone --depth 1 --branch v0.3.0 https://github.com/mapbox/mason.git ./.mason;
 export PATH=$(pwd)/.mason:${PATH};
@@ -11,9 +11,7 @@ mason install clang 3.8.0;
 export PATH=$(mason prefix clang 3.8.0)/bin:${PATH};
 export CXX=clang++-3.8;
 export CC=clang-3.8;
-export PYTHONPATH=$(pwd)/mason_packages/.link/lib/python2.7/site-packages;
 export PATH=./node_modules/.bin/:$PATH
-
 
 ./scripts/build_against_sdk.sh
 
